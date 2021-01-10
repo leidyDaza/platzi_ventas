7341ae7eb8805f9641ce5ee56dcf055448fe0a11
Leidy Daza <leidydaza@unimayor.edu.co>
2021-01-10 12:54:26 -0500

    Clase 12 uso de condicionales en nuestro proyecto

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ clients ='pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients+=client_name
-    _add_comma()
+    if client_name not in clients:
+        clients+=client_name
+        _add_comma()
+    else:
+        print('Client already is the client\'s list') 
+    
 def list_clients():
     global clients
     print(clients)    
@@ -12,7 +16,25 @@ def _add_comma():
     global clients
     clients+=','
 
+def __print_welcome():
+    print("Bienvenidos a platzi ventas")
+    print("*"*50)
+    print('What would you like to do today?')#que es lo que quieres hacer hoy
+    print('[C]reate client')
+    print('[D]elete client')
+
 if __name__=='__main__':# este es el punto de entrada
+    __print_welcome()
+
+    command=input()
+
+    if command=='C':
+        client_name=input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command =='D':
+        pass
+    else:
+        print('Invalid command')
     list_clients()
-    create_client('David')
-    list_clients()
+    

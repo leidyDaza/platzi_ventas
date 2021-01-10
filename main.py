7142cf99ba05556035f8ed1e0a5bbed00b0aecea
Leidy Daza <leidydaza@unimayor.edu.co>
2021-01-10 12:06:10 -0500

    Usando funciones en nuestro proyecto para añadir y mostrar clientes

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-clients='pablo,ricardo,'
+clients ='pablo,ricardo,'
 
+def create_client(client_name):
+    global clients
+    clients+=client_name
+    _add_comma()
+def list_clients():
+    global clients
+    print(clients)    
+
+def _add_comma():
+    global clients
+    clients+=','
 
 if __name__=='__main__':# este es el punto de entrada
-    clients+='david'
-    print(clients)
+    list_clients()
+    create_client('David')
+    list_clients()
